259a1795bffd78c670bf186d899f985137588571
David <davidarenwick@gmail.com>
2022-03-21 15:48:48 +0000

    Fixed error with button and can now scroll

--- screens/Testscreen.jsx
+++ screens/Testscreen.jsx
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs, addDoc } from 'firebase/firestore';
 
-import { View, Text, Button, TextInput } from 'react-native';
+import {
+  View, Text, Button, TextInput, SafeAreaView, ScrollView, StyleSheet, StatusBar,
+} from 'react-native';
 import { db } from '../firebase';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+});
+
 function Testscreen() {
-  const [newName, setNewName]= useState("")
-  const [newUsername, setNewUsername]= useState("")
+  const [newName, setNewName] = useState('');
+  const [newUsername, setNewUsername] = useState('');
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, 'users');
 
-  const createUser = ()=>{
-    addDoc(usersCollectionRef, {name: newName, username: newUsername})
-  }
+  const createUser = () => {
+    addDoc(usersCollectionRef, { name: newName, username: newUsername });
+  };
 
   useEffect(() => {
     const getUsers = () => {
       getDocs(usersCollectionRef).then((data) => {
         setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        console.log(data)
+        console.log(data);
       });
     };
     getUsers();
   }, []);
 
   return (
-    <View>
-      <TextInput placeholder="Name.." onChange={(e) => { setNewName(e.target.value)}}/>
-      <TextInput placeholder="Username.." onChange={(e) => { setNewUsername(e.target.value)}}/>
-      <Button onPress={createUser}> Create User</Button>
-      {users.map((user) => (
-        <View key={user.id}>
-          <Text>{user.name}</Text>
-          <Text>{user.username}</Text>
-          {/* <Text>{user.preferred_sports[0]}</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View>
+
+          <TextInput placeholder="Name.." onChange={(e) => { setNewName(e.target.value); }} />
+          <TextInput placeholder="Username.." onChange={(e) => { setNewUsername(e.target.value); }} />
+          <Button onPress={createUser} title="Create User"> </Button>
+          {users.map((user) => (
+            <View key={user.id}>
+              <Text>{user.name}</Text>
+              <Text>{user.username}</Text>
+              {/* <Text>{user.preferred_sports[0]}</Text>
           <Text>{user.date_joined.toString()}</Text> */}
+            </View>
+          ))}
+
         </View>
-      ))}
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
