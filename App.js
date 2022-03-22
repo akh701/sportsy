f9b68e3f44f840593488e46d637b46b3eed100f6
Andrew Kennedy <andrew.kennedy27@outlook.com>
2022-03-22 10:30:59 +0000

    added login/registration, profile page, react navigation

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 
-import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
-import Testscreen from "./screens/Testscreen"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import UserProfile from './screens/UserProfile';
+
+const Stack = createNativeStackNavigator();
 
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Testscreen />
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<NavigationContainer>
+      		<Stack.Navigator>
+        		<Stack.Screen name="Login" options={{ headerShown: false }} component={LoginScreen} />
+        		<Stack.Screen name="Profile" component={UserProfile} />
+      		</Stack.Navigator>
+    	</NavigationContainer>
 	)
 }
 
