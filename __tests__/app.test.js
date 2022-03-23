d38e0c6da247cde2dc1155a1d87b2da077400227
root <root@DESKTOP-V8A11UG.localdomain>
2022-03-23 15:18:13 +0000

    add testing for mock cloud database get requests.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,28 +1,3 @@
-// // eslint-disable-next-line import/no-unresolved
-// import firebase from 'firebase';
-// import { firestore } from '../test-helper';
-
-// firebase.firestore = firestore;
-
-// describe('setDocData', () => {
-//   const mockData = { fake: 'data' };
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//     setDocData('fakeDocID', mockData);
-//   });
-
-//   it('writes the correct doc', () => {
-//     expect(firestore().doc).toHaveBeenCalledWith('docs/fakeDocID');
-//   });
-
-//   it('adds a timestamp, and writes it to the doc', () => {
-//     expect(firestore().doc().set).toHaveBeenCalledWith({
-//       created: 'MOCK_TIME',
-//       fake: 'data',
-//     });
-//   });
-// });
-
 const { mockGoogleCloudFirestore } = require('firestore-jest-mock');
 
 mockGoogleCloudFirestore({
@@ -35,22 +10,148 @@ mockGoogleCloudFirestore({
         id: 2, DOB: 'December 29, 1994', name: 'Aeneas Sheenan', avatar: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582__340.png', dateJoined: '22 March 2022 at 10:13:00 UTC', location: 'SR7 7UG', preferredSports: ['Football', 'Tennis'], username: 'Aenan90',
       },
       {
-        id: 3, DOB: 'December 29, 1994', name: 'Aeneas Sheenan', avatar: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582__340.png', dateJoined: '22 March 2022 at 10:13:00 UTC', location: 'SR7 7UG', preferredSports: ['Football', 'Tennis'], username: 'Aenan90',
-      }
+        id: 3, DOB: 'September 29, 1994', name: 'David Renwick', avatar: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464__340.png', dateJoined: '22 March 2022 at 10:17:00 UTC', location: 'L25 2SE', preferredSports: ['Football', 'Tennis'], username: 'BritHorv56546764565',
+      },
     ],
   },
 });
 
-const { mockCollection } = require('firestore-jest-mock/mocks/firestore');
+beforeEach(() => jest.clearAllMocks());
 
-test('expect users collection to be returned from firestore', () => {
-  const { Firestore } = require('@google-cloud/firestore');
-  const firestore = new Firestore();
+const { mockCollection, mockWhere, mockLimit, mockOrderBy } = require('firestore-jest-mock/mocks/firestore');
 
-  return firestore.collection('users').get().then((userDocs) => {
-    console.log(userDocs.docs[0].data());
-    expect(mockCollection).toHaveBeenCalledWith('users');
-    expect(userDocs.docs[0].data().name).toEqual('Félix Schultheiss')
-    
+describe('testing get requests to users table', () => {
+  test('expect users collection to be returned from firestore', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+
+    return firestore.collection('users').get().then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(userDocs.docs[0].data().name).toEqual('Félix Schultheiss');
+    });
+  });
+  test('query with name', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+    function getUser(name) {
+      let query = firestore.collection('users');
+
+      if (name) {
+        query = query.where('name', '==', name);
+      }
+
+      return query.get();
+    }
+    return getUser('Félix Schultheiss').then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(mockWhere).toHaveBeenCalledWith('name', '==', 'Félix Schultheiss');
+    });
+  });
+  test('query users without name', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+    function getUser(name) {
+      let query = firestore.collection('users');
+
+      if (name) {
+        query = query.where('name', '==', name);
+      }
+
+      return query.get();
+    }
+    return getUser().then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(mockWhere).not.toHaveBeenCalled();
+    });
+  });
+  test('query users with limit of 2', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+    function getUser(name, limit) {
+      let query = firestore.collection('users');
+
+      if (name) {
+        query = query.where('name', '==', name);
+      }
+      if (limit) {
+        query = query.limit(limit);
+      }
+      return query.get();
+    }
+    return getUser(null, 2).then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(mockWhere).not.toHaveBeenCalled();
+      expect(mockLimit).toHaveBeenCalled();
+    });
+  });
+  test('query users with order by name desc', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+    function getUser(name, limit, orderBy, order) {
+      let query = firestore.collection('users');
+
+      if (name) {
+        query = query.where('name', '==', name);
+      }
+      if (limit) {
+        query = query.limit(limit);
+      }
+      if (orderBy) {
+        query = query.orderBy(orderBy, order);
+      }
+      return query.get();
+    }
+    return getUser(null, null, 'name', 'desc').then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(mockWhere).not.toHaveBeenCalled();
+      expect(mockLimit).not.toHaveBeenCalled();
+      expect(mockOrderBy).toHaveBeenCalled();
+    });
+  });
+  test('query users with preferredsports array contains tennis or football', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+    function getUser(sports, name, limit, orderBy, order) {
+      let query = firestore.collection('users');
+      if (name) {
+        query = query.where('name', '==', name);
+      }
+      if(sports) {
+        query = query.where('preferredSports', 'array-contains-any', sports)
+      }
+      if (limit) {
+        query = query.limit(limit);
+      }
+      if (orderBy) {
+        query = query.orderBy(orderBy, order);
+      }
+      return query.get();
+    }
+    return getUser(['Football', 'Tennis']).then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(mockWhere).toHaveBeenCalled();
+      expect(mockLimit).not.toHaveBeenCalled();
+      expect(mockOrderBy).not.toHaveBeenCalled();
+    });
+  });
+  test('query users with multiple where clauses', () => {
+    const { Firestore } = require('@google-cloud/firestore');
+    const firestore = new Firestore();
+    function getUser(name, username) {
+      let query = firestore.collection('users');
+      if (name) {
+        query = query.where('name', '==', name);
+      }
+      if(username) {
+        query = query.where('username', '==', username)
+      }
+      return query.get();
+    }
+    return getUser('Félix Schultheiss', 'Schulty23').then((userDocs) => {
+      expect(mockCollection).toHaveBeenCalledWith('users');
+      expect(mockWhere).toHaveBeenCalledTimes(2);
+      expect(mockLimit).not.toHaveBeenCalled();
+      expect(mockOrderBy).not.toHaveBeenCalled();
+    });
   });
 });
