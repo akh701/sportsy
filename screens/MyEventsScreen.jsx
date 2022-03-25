1c7005c65245b62ae005014fab347e4ea190180e
David <davidarenwick@gmail.com>
2022-03-25 14:54:20 +0000

    Query now working

--- screens/MyEventsScreen.jsx
+++ screens/MyEventsScreen.jsx
@@ -1,8 +1,45 @@
-import * as React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Text } from 'react-native';
-// import Header from './globalScreens/HeaderComponent';
+import {
+  collection, query, where, getDocs,
+} from 'firebase/firestore';
+import { db, auth } from '../firebase';
 
 export default function MyEventsScreen({ navigation }) {
+  const [userCreatedEvents, setUserCreatedEvents] = useState([]);
+  const [userAttendingEvents, setUserAttendingEvents] = useState([]);
+  const [isloading, setLoading] = useState(true);
+  const createdEventsQuery = query(collection(db, 'events'), where('creatorId', '==', auth.currentUser.uid));
+  const attendingEventsQuery = query(collection(db, 'events'), where('attendees', '==', auth.currentUser.uid));
+
+  /// USE EFFECT TO GET EVENTS CREATED BY USER
+  useEffect(() => {
+    setLoading(true);
+
+    getDocs(createdEventsQuery).then((events) => {
+      const eventsArray = [];
+      events.forEach((event) => {
+        eventsArray.push(event.data());
+      });
+      setUserCreatedEvents(eventsArray);
+      setLoading(false);
+    });
+  }, []);
+
+  /// USE EFFECT TO GET EVENTS CREATED BY USER
+  useEffect(() => {
+    setLoading(true);
+
+    getDocs(q).then((events) => {
+      const eventsArray = [];
+      events.forEach((event) => {
+        eventsArray.push(event.data());
+      });
+      setUserCreatedEvents(eventsArray);
+      setLoading(false);
+    });
+  }, []);
+
   return (
 
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
